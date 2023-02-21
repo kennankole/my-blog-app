ca018107fa66c86459cfb2c078f271ac64bd250f
kennankole <kennankole@gmail.com>
2023-02-21 13:59:55 +0300

    Added index files to all columns with foreign key

--- db/migrate/20230221104354_add_user_ref_to_posts.rb
+++ db/migrate/20230221104354_add_user_ref_to_posts.rb
@@ -1,5 +1,6 @@
 class AddUserRefToPosts < ActiveRecord::Migration[7.0]
   def change
     add_reference :posts, :author, null: false, foreign_key: true
+    add_index :posts, :author
   end
 end
--- db/migrate/20230221104512_add_user_ref_to_likes.rb
+++ db/migrate/20230221104512_add_user_ref_to_likes.rb
@@ -1,5 +1,6 @@
 class AddUserRefToLikes < ActiveRecord::Migration[7.0]
   def change
     add_reference :likes, :author, null: false, foreign_key: true
+    add_index :likes, :author
   end
 end
