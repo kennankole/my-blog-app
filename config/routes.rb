3ec4567b6608096224972490bf46b5e939ce87a6
kennankole <kennankole@gmail.com>
2023-03-09 22:08:50 +0300

    Styled user registration and login forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'users#index'
-  devise_for :users, controllers: { confirmations: 'confirmations' } 
+  devise_for :users, controllers: { confirmations: 'users/confirmations' } 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,18 +5,8 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*',
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-  ]
+  theme: {},
+  plugins: [],
 }
