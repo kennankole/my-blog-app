e10ac4ddbcc4a62c40a282fa2765def4dfdd4a49
kennankole <kennankole@gmail.com>
2023-03-10 18:04:50 +0300

    Refactored the routes api routes

--- config/routes.rb
+++ config/routes.rb
@@ -23,10 +23,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users do
-        resources :posts do
-          resources :comments
+      resources :users, only: [:index] do
+        resources :posts, only: [:index, :show] do
+          resources :comments, only: [:index, :create]
         end
+        resources :comments, only: [:index]
       end
     end
   end
